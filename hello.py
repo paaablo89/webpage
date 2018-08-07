24e70cd95df5eb6ad12574eb27ebed931981c4e3
Pablo Moyano <pablo.moyano89@gmail.com>
2018-08-07 12:08:37 -0300

    Laptop: push modificado

--- hello.py
+++ hello.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello guardian, Im your Ghost!'
+    return 'Hello guardian!!, Im your Ghost!'
+
+@app.route("/user/<user>")
+def user(user):
+    return "Perfil del guardian %s" % user
 
 if __name__ == "__main__":
     app.run()
