19dccf6c4d2cb4469437b1ff8433abdb99d0c0d3
Pablo Moyano <pablo.moyano89@gmail.com>
2018-08-07 12:12:17 -0300

    Laptop: commit v2 con templates

--- hello.py
+++ hello.py
@@ -1,13 +1,20 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
+
+userlist = ['Agustin', 'Guillermo', 'Victor', 'Nacho', 'Pablo', 'Andres']
 
 @app.route('/')
 def hello_world():
-    return 'Hello guardian!!, Im your Ghost!'
+    return render_template("index.html")
+
+@app.route("/user")
+def users():
+    return render_template("users.html", userlist=userlist)
 
 @app.route("/user/<user>")
 def user(user):
-    return "Perfil del guardian %s" % user
+    return render_template("user.html", user=user)
 
 if __name__ == "__main__":
     app.run()
